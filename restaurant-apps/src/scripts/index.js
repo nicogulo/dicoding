21d51ac04495f81a6652daa595b0bbd2de2790f0
nicogulo <nicogulobelas@gmail.com>
2021-04-30 14:19:36 +0700

    styling

--- restaurant-apps/src/scripts/index.js
+++ restaurant-apps/src/scripts/index.js
@@ -12,11 +12,10 @@ data.restaurants.forEach((restaurants) => {
   restoData += `
     <div class="posts">
     <article class="post-item">
-      
-        <img class="post-item__thumbnail" src="${restaurants.pictureId}" alt="Gambar Restorant">
-      
+      <img class="post-item__thumbnail" src="${restaurants.pictureId}" alt="Gambar Restorant">
       <div class="post-item__content">
-        <div class="post-item__title">${restaurants.name}</div>
+        <p class="post-item__title">${restaurants.name}</p>
+        <p class="post-item__city">${restaurants.city}</p>
         <div class="post-item__description">${restaurants.description}</div>
       </div>
       </article>
@@ -24,4 +23,4 @@ data.restaurants.forEach((restaurants) => {
   `;
 });
 
-document.getElementById("card-container").innerHTML = restoData;
+document.getElementById("posts").innerHTML = restoData;
