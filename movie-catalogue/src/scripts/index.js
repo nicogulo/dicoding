92766abd41d51cd62ec99b6e83ca9e9cbb7ab226
nicogulo <nicogulobelas@gmail.com>
2021-05-04 15:27:36 +0700

    styling and import in index.js

--- movie-catalogue/src/scripts/index.js
+++ movie-catalogue/src/scripts/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable quotes */
 import "regenerator-runtime";
+import "../styles/style.css";
+import "../styles/responsive.css";
 
 // eslint-disable-next-line linebreak-style
 console.log("Hello Coders!");
