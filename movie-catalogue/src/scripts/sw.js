fef9f38308fb48575abfecfb09e4b0ffd24b88a3
nicogulo <nicogulobelas@gmail.com>
2021-05-06 09:50:24 +0700

    register service worker

---
+++ movie-catalogue/src/scripts/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  console.log('installing Service Worker..');
+
+  // TODOL Chaching app shell resource
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('activating Service Worker');
+
+  // TODOL Delete old caches
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request);
+
+  e.responWith(fetch(e.request));
+  // TODOL add/get fetch req to/from caches
+});
--- movie-catalogue/webpack.common.js
+++ movie-catalogue/webpack.common.js
@@ -1,15 +1,15 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable quotes */
 /* eslint-disable import/no-extraneous-dependencies */
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// service worker
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/scripts/index.js"),
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -17,10 +17,10 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
         ],
       },
@@ -28,16 +28,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/templates/index.html"),
-      filename: "index.html",
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/public/"),
-          to: path.resolve(__dirname, "dist/"),
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
     }),
   ],
 };
