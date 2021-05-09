52c068d7aaa8eddc313f6931c9fc5c70d9b86210
nicogulo <nicogulobelas@gmail.com>
2021-05-09 20:05:13 +0700

    add notification

--- movie-catalogue/src/scripts/utils/websocket-initiator.js
+++ movie-catalogue/src/scripts/utils/websocket-initiator.js
@@ -1,3 +1,6 @@
+import NotificationHelper from './notification-helper';
+import CONFIG from '../globals/config';
+
 const WebSocketInitiator = {
 
   // method init
@@ -9,7 +12,14 @@ const WebSocketInitiator = {
 
   // callback event onmessage untuk menampilkan data
   _onMessageHandler(message) {
-    console.log(message.data);
+    const movie = JSON.parse(message.data);
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is on cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
   },
 };
 export default WebSocketInitiator;
