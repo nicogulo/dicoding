0b6d55fdecbc2ddeda1fc5403f44db58ec758309
nicogulo <nicogulobelas@gmail.com>
2021-05-11 01:46:53 +0700

    complete detail info

--- inshokuten-resto/src/scripts/views/templates/template-creator.js
+++ inshokuten-resto/src/scripts/views/templates/template-creator.js
@@ -5,8 +5,42 @@ const createRestoDetailTemplate = (restaurant) => `
   <img class="resto__poster" src="${CONFIG.BASE_IMAGE_URL + restaurant.restaurant.pictureId}" alt="${restaurant.restaurant.name}" />
   <div class="resto__info">
   <h3>Information</h3>
-    <h4>City</h4>
-    <p>${restaurant.restaurant.city}</p>
+    <h4>Address</h4>
+    <p>${restaurant.restaurant.address}</p>
+    <h4>Categories</h4>
+    <p>${restaurant.restaurant.categories.map((category) => category.name).join(' - ')}</p>
+    <h4>Food Menu</h4>
+    <p>${restaurant.restaurant.menus.foods
+    .map(
+      (food) => `
+                <li>${food.name}</li>
+              `,
+    ).join('')
+}
+      </p>
+
+    <h4>Drinks Menu</h4>
+    <p>${restaurant.restaurant.menus.drinks
+    .map(
+      (drink) => `
+                <li>${drink.name}</li>
+              `,
+    ).join('')
+}
+      </p>
+
+      <h4>Review</h4>
+    <p>${restaurant.restaurant.customerReviews
+    .map(
+      (review) => `
+                <li>${review.name}</li>
+                <li>${review.review}</li>
+                <li>${review.date}</li>
+              `,
+    ).join('')
+}
+      </p>
+
     <h4>Rating</h4>
     <p>${restaurant.restaurant.rating}</p>
   </div>
