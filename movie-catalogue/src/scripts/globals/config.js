34afe36c280d576d7fa764baf4cebb414c16bde1
nicogulo <nicogulobelas@gmail.com>
2021-05-10 20:12:32 +0700

    add back movie catalogue

---
+++ movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: '525478a2cddf94715e958a782a216ed8',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+};
+
+export default CONFIG;
