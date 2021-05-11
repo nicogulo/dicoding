399dff597d3bb905f5b7311eb781159e69287b62
nicogulo <nicogulobelas@gmail.com>
2021-05-12 00:15:38 +0700

    styling detail

--- inshokuten-resto/src/scripts/views/templates/template-creator.js
+++ inshokuten-resto/src/scripts/views/templates/template-creator.js
@@ -4,49 +4,59 @@ const createRestoDetailTemplate = (restaurant) => `
   <h2 class="resto__title">${restaurant.restaurant.name}</h2>
   <img class="resto__poster" src="${CONFIG.BASE_IMAGE_URL + restaurant.restaurant.pictureId}" alt="${restaurant.restaurant.name}" />
   <div class="resto__info">
-  <h3>Information</h3>
+    <h3>Information</h3>
     <h4>Address</h4>
-    <p>${restaurant.restaurant.address}</p>
-    <h4>Categories</h4>
-    <p>${restaurant.restaurant.categories.map((category) => category.name).join(' - ')}</p>
+      <p>${restaurant.restaurant.address}</p>
+      <h4>Categories</h4>
+      <p>${restaurant.restaurant.categories.map((category) => category.name).join(' - ')}</p>
+          
+      <h4>Rating</h4>
+      <p>${restaurant.restaurant.rating}</p>
+  </div>
+  
+  <div class="resto__overview">
     <h4>Food Menu</h4>
-    <p>${restaurant.restaurant.menus.foods
-    .map(
-      (food) => `
-                <li>${food.name}</li>
-              `,
-    ).join('')
-}
+      <p>${restaurant.restaurant.menus.foods.map((food) => `
+                  <li>${food.name}</li>`).join('')}
       </p>
 
     <h4>Drinks Menu</h4>
-    <p>${restaurant.restaurant.menus.drinks
-    .map(
-      (drink) => `
-                <li>${drink.name}</li>
-              `,
-    ).join('')
-}
+      <p>${restaurant.restaurant.menus.drinks.map((drink) => `
+                    <li>${drink.name}</li>`).join('')}
       </p>
-
-      <h4>Review</h4>
+ 
+    <h3>Description</h3>
+    <p>${restaurant.restaurant.description}</p>
     <p>${restaurant.restaurant.customerReviews
     .map(
       (review) => `
-                <li>${review.name}</li>
-                <li>${review.review}</li>
-                <li>${review.date}</li>
+      <div class="review-container">
+      <h3>Reviews</h3>
+     
+      <div class="cards">
+        <div class="card">
+          <div class="card-top">
+            <div class="name">
+              <div class="img one" alt="">NF</div>
+               <p>${review.name}</p>
+            </div>
+          </div>
+          
+          <div class="card-content">
+            <p>${review.review}</p>
+          </div>
+          
+          <div class="card-action">
+            <span>${review.date}</span>
+          </div>    
+        </div>
+      </div>
+    </div>
               `,
     ).join('')
 }
       </p>
 
-    <h4>Rating</h4>
-    <p>${restaurant.restaurant.rating}</p>
-  </div>
-  <div class="resto__overview">
-    <h3>Description</h3>
-    <p>${restaurant.restaurant.description}</p>
   </div>
 `;
 
