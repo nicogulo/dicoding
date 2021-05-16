ba713e17343334b03bf8a141b4a255cc4a6136a1
nicogulo <nicogulobelas@gmail.com>
2021-05-16 14:46:17 +0700

    fix submission styling

--- inshokuten-resto/src/scripts/index.js
+++ inshokuten-resto/src/scripts/index.js
@@ -1,22 +1,20 @@
-import 'regenerator-runtime';
-import '../styles/style.css';
-import '../styles/responsive.css';
-import App from './views/app';
-import swRegister from './utils/sw-register';
-import WebSocketInitiator from './utils/websocket-initiator';
-import CONFIG from './globals/config';
+import "regenerator-runtime";
+import "../styles/style.css";
+import "../styles/responsive.css";
+import App from "./views/app";
+import swRegister from "./utils/sw-register";
 
 const app = new App({
-  button: document.querySelector('#hamburgerButton'),
-  drawer: document.querySelector('#navigationDrawer'),
-  content: document.querySelector('#mainContent'),
+  button: document.querySelector("#hamburgerButton"),
+  drawer: document.querySelector("#navigationDrawer"),
+  content: document.querySelector("#mainContent"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   app.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   app.renderPage();
   // SW
   swRegister();
