1396b4c5a22eb83cf239c95266119884bee44070
nicogulo <nicogulobelas@gmail.com>
2021-06-04 08:07:23 +0700

    mengurangi keterkaitan LikeButtonPresenter dengan FavoriteMovieIdb

--- movie-catalogue-v2/src/scripts/utils/like-button-presenter.js
+++ movie-catalogue-v2/src/scripts/utils/like-button-presenter.js
@@ -1,12 +1,12 @@
-import FavoriteMovieIdb from '../data/favorite-movie-idb';
 import {
   createLikeMovieButtonTemplate,
   createUnlikeMovieButtonTemplate,
 } from '../views/templates/template-creator';
 const LikeButtonPresenter = {
-  async init({ likeButtonContainer, movie }) {
+  async init({ likeButtonContainer, favoriteMovies, movie }) {
     this._likeButtonContainer = likeButtonContainer;
     this._movie = movie;
+    this._favoriteMovies = favoriteMovies;
 
     await this._renderButton();
   },
@@ -22,7 +22,7 @@ const LikeButtonPresenter = {
   },
 
   async _isMovieExist(id) {
-    const movie = await FavoriteMovieIdb.getMovie(id);
+    const movie = await this._favoriteMovies.getMovie(id);
     return !!movie;
   },
 
@@ -31,7 +31,7 @@ const LikeButtonPresenter = {
 
     const likeButton = document.querySelector('#likeButton');
     likeButton.addEventListener('click', async () => {
-      await FavoriteMovieIdb.putMovie(this._movie);
+      await this._favoriteMovies.putMovie(this._movie);
       this._renderButton();
     });
   },
@@ -41,7 +41,7 @@ const LikeButtonPresenter = {
 
     const likeButton = document.querySelector('#likeButton');
     likeButton.addEventListener('click', async () => {
-      await FavoriteMovieIdb.deleteMovie(this._movie.id);
+      await this._favoriteMovies.deleteMovie(this._movie.id);
       this._renderButton();
     });
   },
