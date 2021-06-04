61225cdb70c2e395acf8892720512d80e31fe54e
nicogulo <nicogulobelas@gmail.com>
2021-06-04 09:47:29 +0700

    jangan bertele tele

---
+++ movie-catalogue-v2/specs/favoriteMovieSearchSpec.js
@@ -0,0 +1,90 @@
+import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+
+describe('Searching movies', () => {
+  let presenter;
+
+  const searchMovies = (query) => {
+    const queryElement = document.getElementById('query');
+    queryElement.value = query;
+    queryElement.dispatchEvent(new Event('change'));
+  };
+
+  const setMovieSearchContainer = () => {
+    document.body.innerHTML = `
+        <div id="movie-search-container">
+            <input id="query" type="text">
+            <div class="movie-result-container">
+                <ul class="movies">
+                </ul>
+            </div>
+        </div>
+        `;
+  };
+
+  const constructPresenter = () => {
+    spyOn(FavoriteMovieIdb, 'searchMovies');
+    presenter = new FavoriteMovieSearchPresenter({
+      favoriteMovies: FavoriteMovieIdb,
+    });
+  };
+
+  beforeEach(() => {
+    setMovieSearchContainer();
+    constructPresenter();
+  });
+
+  it('should be able to capture the query typed by the user', () => {
+    searchMovies('film a');
+
+    expect(presenter.latestQuery).toEqual('film a');
+  });
+
+  it('should ask the model to search for movies', () => {
+    searchMovies('film a');
+
+    expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith('film a');
+  });
+
+  it('should show the found movies', () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+    expect(document.querySelectorAll('.movie').length).toEqual(1);
+
+    presenter._showFoundMovies([
+      { id: 1, title: 'Satu' },
+      { id: 2, title: 'Dua' },
+    ]);
+    expect(document.querySelectorAll('.movie').length).toEqual(2);
+  });
+
+  it('should show the title of the found movies', () => {
+    presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
+    expect(
+      document.querySelectorAll('.movie__title').item(0).textContent
+    ).toEqual('Satu');
+  });
+
+  it('should show the title of the found movies', () => {
+    presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
+    expect(
+      document.querySelectorAll('.movie__title').item(0).textContent
+    ).toEqual('Satu');
+
+    presenter._showFoundMovies([
+      { id: 1, title: 'Satu' },
+      { id: 2, title: 'Dua' },
+    ]);
+
+    const movieTitles = document.querySelectorAll('.movie__title');
+    expect(movieTitles.item(0).textContent).toEqual('Satu');
+    expect(movieTitles.item(1).textContent).toEqual('Dua');
+  });
+
+  it('should show - for found movie without title', () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+
+    expect(
+      document.querySelectorAll('.movie__title').item(0).textContent
+    ).toEqual('-');
+  });
+});
---
+++ movie-catalogue-v2/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -0,0 +1,38 @@
+class FavoriteMovieSearchPresenter {
+  constructor({ favoriteMovies }) {
+    this._listenToSearchRequestByUser();
+    this._favoriteMovies = favoriteMovies;
+  }
+
+  _listenToSearchRequestByUser() {
+    this._queryElement = document.getElementById('query');
+    this._queryElement.addEventListener('change', (event) => {
+      this._searchMovies(event.target.value);
+    });
+  }
+
+  _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery;
+    this._favoriteMovies.searchMovies(this.latestQuery);
+  }
+
+  _showFoundMovies(movies) {
+    const html = movies.reduce(
+      (carry, movie) =>
+        carry.concat(
+          `<li class="movie"><span class="movie__title">${
+            movie.title || '-'
+          }</span></li>`
+        ),
+      ''
+    );
+
+    document.querySelector('.movies').innerHTML = html;
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
+  }
+}
+
+export default FavoriteMovieSearchPresenter;
