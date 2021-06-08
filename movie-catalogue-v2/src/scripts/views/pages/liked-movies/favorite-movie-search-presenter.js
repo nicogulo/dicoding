50846bf75fb580be96317a37176d9895bb4a2184
nicogulo <nicogulobelas@gmail.com>
2021-06-08 09:49:35 +0700

    memperbaiki dia yang terlupakan

--- movie-catalogue-v2/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ movie-catalogue-v2/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -1,19 +1,36 @@
-class FavoriteMovieShowPresenter {
-  constructor({ view, favoriteMovies }) {
+class FavoriteMovieSearchPresenter {
+  constructor({ favoriteMovies, view }) {
     this._view = view;
+    this._listenToSearchRequestByUser();
     this._favoriteMovies = favoriteMovies;
-
-    this._showFavoriteMovies();
   }
 
-  async _showFavoriteMovies() {
-    const movies = await this._favoriteMovies.getAllMovies();
-    this._displayMovies(movies);
+  _listenToSearchRequestByUser() {
+    this._view.runWhenUserIsSearching((latestQuery) => {
+      this._searchMovies(latestQuery);
+    });
   }
 
-  _displayMovies(movies) {
+  async _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery.trim();
+
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
+
+    this._showFoundMovies(foundMovies);
+  }
+
+  _showFoundMovies(movies) {
     this._view.showFavoriteMovies(movies);
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
   }
 }
 
-export default FavoriteMovieShowPresenter;
+export default FavoriteMovieSearchPresenter;
